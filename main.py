80af4b47d9e043fae33203ad0788ae72469390cd
Debdut Goswami <debdutgoswami@gmail.com>
2020-01-03 00:10:22 +0530

    v1.1

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 #custom imports
-import username, certificate, re
+import username,  re
 import ProfileImage as pi
 
 url = username.user_url()
-usname = usname = re.findall("https:\/\/www.instagram.com\/([\S]+?)\/", url)[0]
-ctx = certificate.context_ssl() #this step is not mandatory but advicable
+usname = re.findall(r"https:\/\/www.instagram.com\/([\S]+?)\/", url)[0]
 
-profile_pic_url_hd = pi.image_url(url, ctx)
-pi.image_show(profile_pic_url_hd,usname) #opens the image in a new window and also saves the image in the working directory
+pi.image(url, usname)
